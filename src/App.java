6bf17296550277bf73edd2462c99b3ad416887aa
SegundoPaladines <segundopaladines@gmail.com>
2024-01-20 09:51:35 -0500

    Creeria que ya

--- src/App.java
+++ src/App.java
@@ -7,6 +7,9 @@ import interfaces.OperacionAtomica;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class App extends JFrame {
 
@@ -14,6 +17,7 @@ public class App extends JFrame {
     private JTextField textField2;
     private JComboBox<String> operationComboBox;
     private JButton calculateButton;
+    private JButton mapReduceButton;
     private JLabel resultLabel;
 
     public App() {
@@ -30,19 +34,66 @@ public class App extends JFrame {
         calculateButton = new JButton("Calcular");
         resultLabel = new JLabel("Resultado: ");
 
+        mapReduceButton = new JButton("Ver Ejemplo de Map y Reduce");
+
         // configurar el diseño
-        setLayout(new GridLayout(5, 1));
+        setLayout(new GridLayout(6, 1));
         add(textField1);
         add(textField2);
         add(operationComboBox);
         add(calculateButton);
         add(resultLabel);
+        add(mapReduceButton);
 
-        // agregar el ActionListener al botón
+        // agregar el actionistener a los botones
         calculateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 calcularResultado();
+            }
+        });
+
+        mapReduceButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                String input = "";
+
+                try {
+                    input = JOptionPane.showInputDialog("Ingrese los numeros del array separados por comas");
+                } catch (Exception ex) {
+                    System.out.println("Entrada no Valida");
+                    
+                    return;
+                }
+
+                if(!input.equals("")){
+                    if(input.length()>0){
+                        try {
+                            //Capturar los numeros
+                            String[] str_numbers = input.split(",");
+
+                            //map de los numeros para convertirlos en double
+                            double[] numeros = Arrays.stream(str_numbers)
+                                    .mapToDouble(Double::parseDouble)
+                                    .toArray();
+    
+                            // recoleccion de los numeros
+                            List<Double> num_list = Arrays.stream(numeros)
+                                    .boxed()
+                                    .collect(Collectors.toList());
+
+                            arrayMap(num_list);
+                            arrayReduce(num_list);
+
+                        } catch (Exception ex) {
+                            System.out.println("Entrada no Valida");
+
+                            return ;
+                        }
+                    }
+                }{
+                    System.out.println("Entrada no Valida");
+                }
             }
         });
 
@@ -54,18 +105,13 @@ public class App extends JFrame {
          //instancia de las funciones
         Functions funciones = new Functions();
 
-        //funciones lamda
+        /* FUNCIONES LAMBDA */
 
         //funciones de un solo parametro
-        OperacionAtomica cuadrado = (a) -> funciones.Cuadrado(a);
+        OperacionAtomica cuadrado = null;
         
         //funciones con 2 parametros
-        OperacionBinaria potencia = (a, b) -> funciones.Potencia(a, b);
-        OperacionBinaria raiz = (a, b) -> funciones.Raiz(a, b);
-        OperacionBinaria multiplicacion = (a, b) -> funciones.Multiplicacion(a, b);
-        OperacionBinaria division = (a, b) -> funciones.Divicion(a, b);
-        OperacionBinaria suma = (a,b) -> funciones.Suma(a, b);
-        OperacionBinaria resta = (a, b) -> funciones.Resta(a, b);
+        OperacionBinaria funcion = null;
 
         // la opcion selecionada
         String selectedOperationString = (String) operationComboBox.getSelectedItem();
@@ -89,36 +135,78 @@ public class App extends JFrame {
         double valor1 = Double.parseDouble(textField1.getText());
         double valor2 = Double.parseDouble(textField2.getText());
 
-        double resultado = 0;
-
+        //switch de la operacion
         switch (selectedOperationString) {
             case "Cuadrado":
-                resultado = cuadrado.calcular(valor1);
+                cuadrado =  (a) -> funciones.Cuadrado(a);
                 break;
             case "Suma":
-                resultado = suma.calcular(valor1, valor2);
+                funcion = (a,b) -> funciones.Suma(a, b);
                 break;
             case "Resta":
-                resultado = resta.calcular(valor1, valor2);
+                funcion = (a, b) -> funciones.Resta(a, b);
                 break;
             case "Potencia":
-                resultado = potencia.calcular(valor1, valor2);
+                funcion = (a, b) -> funciones.Potencia(a, b);
                 break;
             case "Raiz":
-                resultado = raiz.calcular(valor1, valor2);
+                funcion = (a, b) -> funciones.Raiz(a, b);
                 break;
             case "Multiplicacion":
-                resultado = multiplicacion.calcular(valor1, valor2);
+                funcion = (a, b) -> funciones.Multiplicacion(a, b);
                 break;
             case "Division":
-                resultado = division.calcular(valor1, valor2);
+                funcion = (a, b) -> funciones.Divicion(a, b);
                 break;
             default:
                 System.out.println("Operación no definida");
         }
 
-        // mostrar el resultado en el JLabel
-        resultLabel.setText("Resultado: " + resultado);
+        if(funcion != null){
+            double resultado = funcion.calcular(valor1, valor2);
+            resultLabel.setText("Resultado: " + resultado);
+            
+            return;
+        }
+        
+        if(cuadrado != null){
+            double resultado = cuadrado.calcular(valor1);
+            resultLabel.setText("Resultado: " + resultado);
+
+            return;
+        }
+
+        resultLabel.setText("Resultado: NaN");
+    }
+
+    public void arrayMap(List<Double> numbers){
+        System.out.println("-----------------------------MAPEO--------------------------------\n");
+
+        System.out.print("Array Original: ");
+        System.out.print(numbers + "\n");
+
+        //realizar el reccorrido de la lista mediante map y multiplicarla por 2
+        List<Double> doubledNumbers = numbers.stream()
+                .map(n -> n * 2)
+                .collect(Collectors.toList());
+
+        System.out.print("Array Mapeado x*2: ");
+        System.out.print(doubledNumbers + "\n");
+
+        System.out.println("------------------------------------------------------------------\n");
+    }
+
+    public void arrayReduce(List<Double> numbers){
+        System.out.println("-----------------------------REDUCE--------------------------------\n");
+        System.out.print("Array Original: ");
+        System.out.print(numbers +"\n");
+        // se usa reduce para sumar todos los numeros
+        double sum = numbers.stream().reduce(0.0, (acc, n) -> acc + n);
+
+        System.out.print("Resultado de la Suma: ");
+        System.out.print(sum + "\n");
+
+        System.out.println("---------------------------------------------------------------\n");
     }
 
     public static void main(String[] args) {
